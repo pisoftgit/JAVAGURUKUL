eb9144ec2c625ec32c0145a5b348da013f2cd395
niharikagoyal <111761903+niharikagoyal@users.noreply.github.com>
2025-08-06 10:03:18 +0530

    Update deploy.sh

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,41 +1,41 @@
-#!/bin/bash
-set -e
+# #!/bin/bash
+# set -e
 
-echo " ----  Code By Kuldeepsaini65  ----- "
-echo "Deployment started ..."
+# echo " ----  Code By Kuldeepsaini65  ----- "
+# echo "Deployment started ..."
 
-# Pull the latest version of the app
-git pull
-echo "New changes copied to server !"
+# # Pull the latest version of the app
+# git pull
+# echo "New changes copied to server !"
 
-# Activate Virtual Env
-source ~/project/venv/bin/activate
-echo "Virtual env 'venv' Activated !"
+# # Activate Virtual Env
+# #source ~/project/venv/bin/activate
+# #echo "Virtual env 'venv' Activated !"
 
-echo "Installing Dependencies..."
-pip install -r requirements.txt --no-input
+# echo "Installing Dependencies..."
+# pip install -r requirements.txt --no-input
 
-echo "Serving Static Files..."
-python manage.py collectstatic --noinput
+# echo "Serving Static Files..."
+# python manage.py collectstatic --noinput
 
-echo "Running Database migrations..."
-python manage.py makemigrations
-python manage.py migrate
+# echo "Running Database migrations..."
+# python manage.py makemigrations
+# python manage.py migrate
 
-# Deactivate Virtual Env
-deactivate
-echo "Virtual env 'venv' Deactivated !"
+# # Deactivate Virtual Env
+# deactivate
+# echo "Virtual env 'venv' Deactivated !"
 
-# Reloading Application So New Changes could reflect on website
-cd ~/project/javagurukul
-touch wsgi.py
-cd -
+# # Reloading Application So New Changes could reflect on website
+# cd ~/project/javagurukul
+# touch wsgi.py
+# cd -
 
-systemctl restart gunicorn
-echo "******Restarted Gunicron*****"
-systemctl restart nginx
-echo "******Restarted Nginx*****"
+# systemctl restart gunicorn
+# echo "******Restarted Gunicron*****"
+# systemctl restart nginx
+# echo "******Restarted Nginx*****"
 
-echo "Deployment Finished!"
+# echo "Deployment Finished!"
 
-echo "Happy Coding...."
+# echo "Happy Coding...."
